ef392089f2a44e6ce317e84561437b27a136f4ee
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 18:12:28 -0300

    16: estrutura de repetição

---
+++ 16_repeticao/js/script.js
@@ -0,0 +1,35 @@
+// WHILE
+var incrementar = 0
+
+while(incrementar < 10) {
+    incrementar++;
+    console.log("while " + incrementar);
+}
+
+var y = 0;
+var arr = ['marcelo', 'alves', 'teste', 'testando'];
+
+while(y < 3) {
+    console.log('while array ' + arr[y]);
+    y++;
+}
+
+// FOR
+
+for(i = 0; i < 10; i++) {
+    console.log('for ' + i);
+}
+
+var arr2 = ['teste', 'batata', 'biscoito','rapadura'];
+
+for(j = 0; j < arr2.length; j++) {
+    console.log('for array ' + arr2[j]);
+}
+
+// DO WHILE
+var c = 0
+
+do{ 
+console.log('do while ' + c)
+c++;
+}while(c < 10);
