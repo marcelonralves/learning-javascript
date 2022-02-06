67371e11ea17c8c4f02e77282557a2e92eb3d689
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 15:29:03 -0300

    14: comparação de tipos

---
+++ 14_comparacao_valor/js/script.js
@@ -0,0 +1,14 @@
+var numero = 5;
+
+if(numero === 5) {
+    console.log("o número é igual a 5");
+}
+
+var numero2 = '5';
+if(numero2 === 5) {
+    console.log("o numero é igual a 5")
+}
+
+if(numero2 !== 5) {
+    console.log("ele é diferente do tipo número 5");
+}
