d6a2dee4477d4aaa2d5ba83022f03ef40123983c
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 19:13:56 -0300

    18: let e const

---
+++ 18_let_const/js/script.js
@@ -0,0 +1,18 @@
+let x = 10;
+const y = 100;
+
+console.log('const ' + y)
+
+console.log('let ' + x);
+
+if(true) {
+
+    const y = 50;
+    console.log('const if ' + y);
+    let x = 20;
+    console.log('let if ' + x);
+
+}
+
+console.log('const pos if '+ y);
+console.log('let pos if ' + x);
