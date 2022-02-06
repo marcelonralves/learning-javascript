0f7fc01f9c4a9fa6468e0bcd891864387347b9f0
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 12:34:39 -0300

    03: particularity

---
+++ 03_particularidades_js/js/script.js
@@ -0,0 +1,20 @@
+console.log('first');
+console.log('last');
+console.log('mais um console log')
+
+var a = 1;
+var a = "teste";
+
+console.log(a);
+
+
+// comentário
+
+/*
+
+comentarios de
+multiplas
+linhas 
+
+
+*/
