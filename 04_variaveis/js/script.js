d33cc236eb81880e3b8f8a13eec6c98539069f61
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 12:47:37 -0300

    04: variables

---
+++ 04_variaveis/js/script.js
@@ -0,0 +1,33 @@
+var teste = 1;
+
+teste = 'Marcelo';
+
+console.log(teste);
+
+// nao faça isso
+nome = "marcelo 2";
+console.log(nome);
+
+var $nome = 'teste';
+var _nome = 'teste2';
+
+console.log($nome);
+console.log(_nome);
+
+//camel case
+var meuPrimeiroNome = 'eminem';
+console.log(meuPrimeiroNome);
+
+
+let teste2 = 1;
+const ola = 2;
+
+console.log(teste2);
+console.log(ola);
+
+
+var MeuNome;
+
+MeuNome = 'marcelo alves';
+
+console.log(MeuNome);
