030943b3020c67ba78da49b45f8c4a2bbc440907
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 18:30:13 -0300

    17: funcões

---
+++ 17_funcoes/js/script.js
@@ -0,0 +1,21 @@
+function primeiraFuncao() 
+{
+    console.log("e aí meu patrao");
+}
+
+primeiraFuncao();
+
+function nomeCompleto(nome, sobrenome)
+{
+    console.log(nome + " " + sobrenome)
+}
+
+nomeCompleto("marcelo", "alves")
+
+function soma(numero1, numero2)
+{
+    return numero1 + numero2;
+}
+
+var somardoisnumeros = soma(1,1);
+console.log(somardoisnumeros);
