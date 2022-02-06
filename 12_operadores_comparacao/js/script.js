1a1422e73747d55ca37183750da7d42591babdbc
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 15:11:40 -0300

    12: operadores de comparação

---
+++ 12_operadores_comparacao/js/script.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var idade2 = 15;
+var possuiCarro = 0;
+var nome = "Marcelo"
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer a carteira de habilitação");
+}
+
+if(idade2 <= 17) {
+    console.log("O usuário não pode fazer a carteira de habilitação")
+}
+
+if(possuiCarro) {
+    console.log("O usuário já pode andar de carro");
+}
+
+if(nome = "Zeus") {
+    console.log("O seu nome é " + nome);
+}
+
+if(nome != "Marcelo") {
+    console.log("O seu nome não é Marcelo!");
+}
+
+if(20 > 10) {
+    console.log("Passou");
+}
+
+if(100 < 200) {
+    console.log("Passou 2");
+}
