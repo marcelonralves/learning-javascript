d5e1e8d3868a57d6babe4fca72f16ecf947e62e6
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 15:02:05 -0300

    11: if

---
+++ 11_if/js/script.js
@@ -0,0 +1,10 @@
+var idade = 19;
+var idadeMinima = 18
+
+if(idade > 18) {
+    console.log("Você pode fazer a carteira de habilitação");
+}
+
+if(idade > idadeMinima) {
+    console.log("Você pode fazer a carteira de habilitação");
+}
