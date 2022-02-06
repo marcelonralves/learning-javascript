3733f3e0a1e594209ece69ab77cf392aceeef1f1
Marcelo Alves <marcelonralves@gmail.com>
2022-02-06 15:23:16 -0300

    13: else e else if

---
+++ 13_else_elseif/js/script.js
@@ -0,0 +1,16 @@
+var nome = "Marcelo"
+var idade = 20;
+
+if(nome == "Zeus") {
+    console.log("O nome dele é Zeus");
+} else if(nome == "Marcelo") {
+        console.log("O nome dele é Marcelo!");
+} else {
+    console.log("Ele possui outro nome!");
+}
+a
+if(idade >=20){
+    console.log("Só pode entrar na festa");
+} else if(idade >=18){
+    console.log("Só pode entrar com autorização");
+}
